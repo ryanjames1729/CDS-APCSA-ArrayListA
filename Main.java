7b3134e9e6d9415f6538bdddd2fad2c5fa6e42bd
Ryan James <ryanjames1729@gmail.com>
2025-01-22 11:05:23 +0000

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+// ArrayList 5.10      A+CR
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    //don't forget to make a Scanner and an ArrayList
+  }
+}
